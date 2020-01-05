221d1ec8ed9ed1729be039d5d6fc665b38392d5e
ARXChrono <ChronoXP@hotmail.com>
2020-01-05 16:31:46 +0800

    useSpring to animate onClick and hover events

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,26 @@
 import React, { useState } from "react";
 import { Canvas } from "react-three-fiber";
+import { useSpring, a } from "react-spring/three";
 import "./style.css";
 
 const Box = () => {
   const [hovered, setHovered] = useState(false);
+  const [active, setActive] = useState(false);
+  const props = useSpring({
+    scale: active ? [1.5, 1.5, 1.5] : [1, 1, 1],
+    color: hovered ? "crimson" : "gray"
+  });
 
   return (
-    <mesh
+    <a.mesh
       onPointerOver={() => setHovered(true)}
       onPointerOut={() => setHovered(false)}
+      onClick={() => setActive(!active)}
+      scale={props.scale}
     >
       <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-      <meshBasicMaterial
-        attach="material"
-        color={hovered ? "crimson" : "gray"}
-      />
-    </mesh>
+      <a.meshBasicMaterial attach="material" color={props.color} />
+    </a.mesh>
   );
 };
 
