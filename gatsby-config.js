e3e1bc493a1706b05883f53dfd8f11352e2c38e5
ARXChrono <ChronoXP@hotmail.com>
2020-01-05 22:31:44 +0800

    add manifest theme color, add link hovers

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,18 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `LetsEat`,
+        short_name: `LetsEat`,
+        start_url: `/`,
+        background_color: `#ff4757`,
+        theme_color: `#ff4757`,
+        display: `standalone`,
+      },
+    },
+  ],
 }
