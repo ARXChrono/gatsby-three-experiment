9944a984e142f49022e9982fcd0cf7f2568e8a92
ARXChrono <ChronoXP@hotmail.com>
2020-01-05 18:22:58 +0800

    add prettier, add shadows and fog

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,19 @@
-import React, { useState, useRef } from "react";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { Canvas, useRender, extend, useThree } from "react-three-fiber";
-import { useSpring, a } from "react-spring/three";
-import "./style.css";
+import React, { useState, useRef } from "react"
+import * as THREE from "three"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+import { Canvas, useRender, extend, useThree } from "react-three-fiber"
+import { useSpring, a } from "react-spring/three"
+import "./style.css"
 
-extend({ OrbitControls });
+extend({ OrbitControls })
 
 const Controls = () => {
-  const orbitRef = useRef();
-  const { camera, gl } = useThree();
+  const orbitRef = useRef()
+  const { camera, gl } = useThree()
 
   useRender(() => {
-    orbitRef.current.update();
-  });
+    orbitRef.current.update()
+  })
 
   return (
     <orbitControls
@@ -22,26 +23,26 @@ const Controls = () => {
       args={[camera, gl.domElement]}
       ref={orbitRef}
     />
-  );
-};
+  )
+}
 
 const Plane = () => {
   return (
-    <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -1, 0]}>
+    <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.5, 0]}>
       <planeBufferGeometry attach="geometry" args={[100, 100]} />
       <meshPhysicalMaterial attach="material" color="gray" />
     </mesh>
-  );
-};
+  )
+}
 
 const Box = () => {
-  const meshRef = useRef();
-  const [hovered, setHovered] = useState(false);
-  const [active, setActive] = useState(false);
+  const meshRef = useRef()
+  const [hovered, setHovered] = useState(false)
+  const [active, setActive] = useState(false)
   const props = useSpring({
     scale: active ? [1.5, 1.5, 1.5] : [1, 1, 1],
-    color: hovered ? "crimson" : "gray"
-  });
+    color: hovered ? "crimson" : "gray",
+  })
 
   return (
     <a.mesh
@@ -50,19 +51,27 @@ const Box = () => {
       onPointerOut={() => setHovered(false)}
       onClick={() => setActive(!active)}
       scale={props.scale}
+      castShadow
     >
       <ambientLight />
-      <spotLight position={[0, 5, 10]} />
+      <spotLight castShadow position={[0, 5, 10]} penumbra={1} />
       <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
       <a.meshPhysicalMaterial attach="material" color={props.color} />
     </a.mesh>
-  );
-};
+  )
+}
 
 export default () => (
-  <Canvas>
+  <Canvas
+    camera={[0, 0, 5]}
+    onCreated={({ gl }) => {
+      gl.shadowMap.enabled = true
+      gl.shadowMap.type = THREE.PCFSoftShadowMap
+    }}
+  >
+    <fog attach="fog" args={["white", 10, 20]} />
     <Controls />
     <Box />
     <Plane />
   </Canvas>
-);
+)
