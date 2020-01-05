3bc841b0bf2b21320d6253c95e734033b634f2d8
ARXChrono <ChronoXP@hotmail.com>
2020-01-05 16:06:17 +0800

    i made a sqaure

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,4 +2,10 @@ import React from "react";
 import { Canvas } from "react-three-fiber";
 import "./style.css";
 
-export default () => <Canvas />;
+export default () => (
+  <Canvas>
+    <mesh>
+      <boxBufferGeometry attach="geometry" arcs={[1, 1, 1]} />
+    </mesh>
+  </Canvas>
+);
