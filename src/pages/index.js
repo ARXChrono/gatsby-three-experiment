35124b3ba27bae03def6eb55a5fb7099cd8510ec
ARXChrono <ChronoXP@hotmail.com>
2020-01-05 16:45:23 +0800

    add mesh ref. rotate mesh with useRender

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
-import { Canvas } from "react-three-fiber";
+import React, { useState, useRef } from "react";
+import { Canvas, useRender } from "react-three-fiber";
 import { useSpring, a } from "react-spring/three";
 import "./style.css";
 
 const Box = () => {
+  const meshRef = useRef();
   const [hovered, setHovered] = useState(false);
   const [active, setActive] = useState(false);
   const props = useSpring({
@@ -11,8 +12,13 @@ const Box = () => {
     color: hovered ? "crimson" : "gray"
   });
 
+  useRender(() => {
+    meshRef.current.rotation.y += 0.01;
+  });
+
   return (
     <a.mesh
+      ref={meshRef}
       onPointerOver={() => setHovered(true)}
       onPointerOut={() => setHovered(false)}
       onClick={() => setActive(!active)}
