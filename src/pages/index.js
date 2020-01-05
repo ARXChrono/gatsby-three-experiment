c0de69b056e30307d00b31c78046f568a90d9868
ARXChrono <ChronoXP@hotmail.com>
2020-01-05 16:15:49 +0800

    convert to component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,17 @@ import React from "react";
 import { Canvas } from "react-three-fiber";
 import "./style.css";
 
-export default () => (
-  <Canvas>
+const Box = () => {
+  return (
     <mesh>
       <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
       <meshBasicMaterial attach="material" color="crimson" />
     </mesh>
+  );
+};
+
+export default () => (
+  <Canvas>
+    <Box />
   </Canvas>
 );
